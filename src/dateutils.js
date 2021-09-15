3d9331b730c6afa0ce97d256540c2d78c55ff281
Justin Prince <justinkprince@gmail.com>
2021-09-14 21:29:09 -0400

    Swap date libraries being used for all components.

--- src/dateutils.js
+++ src/dateutils.js
@@ -3,18 +3,18 @@
  * To switch to use date-fns as the date library, comment out the this block and uncomment the Luxon block.
  */
 
-// const { formatDistance, subDays } = require("date-fns");
-// const getRelativeDaysAgo = (daysAgo) =>
-//   formatDistance(subDays(new Date(), daysAgo), new Date(), { addSuffix: true });
+const { formatDistance, subDays } = require("date-fns");
+const getRelativeDaysAgo = (daysAgo) =>
+  formatDistance(subDays(new Date(), daysAgo), new Date(), { addSuffix: true });
 
 /**
  * Luxon
  * To switch to use Luxon as the date library, comment out the date-fns block and uncomment the following block.
  */
 
-const { DateTime } = require("luxon");
-const now = DateTime.local();
-const getRelativeDaysAgo = (daysAgo) =>
-  now.minus({ days: daysAgo }).toRelativeCalendar();
+// const { DateTime } = require("luxon");
+// const now = DateTime.local();
+// const getRelativeDaysAgo = (daysAgo) =>
+//   now.minus({ days: daysAgo }).toRelativeCalendar();
 
 module.exports = getRelativeDaysAgo;
